c133fcf7982087f4f6b43dffa31e32bddf4443e8
Isale <isale.bryan@gmail.com>
2020-09-02 12:42:10 +0300

    Landing Page Skeleton

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+// vue material
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
 
