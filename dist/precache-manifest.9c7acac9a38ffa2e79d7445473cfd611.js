c93aba44bfc59cc5a0b47623e3db33ba86014d88
Isale <isale.bryan@gmail.com>
2020-09-03 09:53:50 +0300

    add files

---
+++ dist/precache-manifest.9c7acac9a38ffa2e79d7445473cfd611.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ffa01a36a822485256",
+    "url": "/css/about.329bf046.css"
+  },
+  {
+    "revision": "f2ab4b251fa23d6132e8",
+    "url": "/css/app.0587a31b.css"
+  },
+  {
+    "revision": "f29dcb3c13b87a5217f9",
+    "url": "/css/chunk-vendors.5699e6ca.css"
+  },
+  {
+    "revision": "fbd37967755a9834016fb5a1b961a9cf",
+    "url": "/img/digipatientt.fbd37967.jpg"
+  },
+  {
+    "revision": "cbf651edf2b8f51bc9ffab050f07d3b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9ffa01a36a822485256",
+    "url": "/js/about.76942f75.js"
+  },
+  {
+    "revision": "f2ab4b251fa23d6132e8",
+    "url": "/js/app.e60ed13d.js"
+  },
+  {
+    "revision": "f29dcb3c13b87a5217f9",
+    "url": "/js/chunk-vendors.6575b3bd.js"
+  },
+  {
+    "revision": "c7239bd460141e47dd806a4b0b261156",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
